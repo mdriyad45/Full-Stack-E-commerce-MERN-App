d2b6eafeec1398e13e712d23ce4a1fba22bad21f
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-05-27 01:18:29 +0600

    bug fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
     emptyOutDir: true
   },
   plugins: [react()],
