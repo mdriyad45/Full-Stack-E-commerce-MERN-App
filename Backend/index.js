2ba49ddfb87bdf8d766cfde5de56ff0e2b25aef1
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-06-01 02:18:33 +0600

    update code

--- Backend/index.js
+++ Backend/index.js
@@ -18,21 +18,9 @@ app.use(
   })
 );
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:5173',
-  'https://frontend-ecommerce-theta-seven.vercel.app'
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
